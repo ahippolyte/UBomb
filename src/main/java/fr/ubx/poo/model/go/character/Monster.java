9123f07cd6b638b1b775ef9fbea5f6d5398efc3b
Raphael Chavagnac <raphael.chavagnac@etu.u-bordeaux.fr>
2021-01-06 12:41:00 +0100

    Version 0.26

--- src/main/java/fr/ubx/poo/model/go/character/Monster.java
+++ src/main/java/fr/ubx/poo/model/go/character/Monster.java
@@ -1,4 +1,29 @@
 package fr.ubx.poo.model.go.character;
 
-public class Monster {
+import fr.ubx.poo.game.Direction;
+import fr.ubx.poo.game.Game;
+import fr.ubx.poo.game.Position;
+import fr.ubx.poo.model.Movable;
+import fr.ubx.poo.model.go.GameObject;
+
+public class Monster extends GameObject implements Movable {
+    private boolean alive = true;
+    Direction direction;
+
+    public Monster(Game game, Position position) {
+        super(game, position);
+        this.direction = Direction.random();
+    }
+
+    public boolean canMove(Direction direction) {
+        return false;
+    }
+    public boolean doMove(Direction direction) {
+        return false;
+    }
+    public void update(long now) {
+        if (canMove(direction)) {
+            doMove(direction);
+        }
+    }
 }
